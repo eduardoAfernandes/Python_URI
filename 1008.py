9549149414a65badc518276d08ba6c9a7221c8e1
eduardoAfernandes <eduardoaugusto2016880@gmail.com>
2021-05-04 21:00:16 -0300

    Exercicio 1008

---
+++ 1008.py
@@ -0,0 +1,10 @@
+number_employee = int(input())
+
+worker_hours = int(input())
+
+hourly_value = float(input())
+
+salary = hourly_value * worker_hours
+
+print("NUMBER =",number_employee)
+print("SALARY = U$ {:.2f}".format(salary))
